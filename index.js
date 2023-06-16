dbba7851fff5e72e4ec2f598181e40921a52e7b9
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-06-16 07:57:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+// Task 1: writeCards function
+function writeCards(names, event) {
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+// Test the writeCards function
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+
+// Task 2: countDown function
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Test the countDown function
+countDown(10);
